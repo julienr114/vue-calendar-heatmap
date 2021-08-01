e17b5e0cd3005a26b1425a43457691dfc13a19f1
Volker Nauruhn <684302+razorness@users.noreply.github.com>
2021-08-02 00:09:33 +0200

    better exports

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -10,16 +10,17 @@ type InstallableComponent = typeof component & { install: Exclude<Plugin['instal
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
 export default /*#__PURE__*/((): InstallableComponent => {
-  // Assign InstallableComponent type
-  const installable = component as unknown as InstallableComponent;
+	// Assign InstallableComponent type
+	const installable = component as unknown as InstallableComponent;
 
-  // Attach install function executed by Vue.use()
-  installable.install = (app: App) => {
-    app.component('Vue3CalendarHeatmap', installable);
-  };
-  return installable;
+	// Attach install function executed by Vue.use()
+	installable.install = (app: App) => {
+		app.component('CalendarHeatmap', installable);
+	};
+	return installable;
 })();
 
 // It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
 // export const RollupDemoDirective = directive;
+export * from './Heatmap';
--- src/entry.ts
+++ src/entry.ts
@@ -4,8 +4,8 @@ import component, * as namedExports from '@/entry.esm';
 // Attach named exports directly to component. IIFE/CJS will
 // only expose one global var, with named exports exposed as properties of
 // that global var (eg. plugin.namedExport)
-Object.entries(namedExports).forEach(([exportName, exported]) => {
-  if (exportName !== 'default') component[exportName] = exported;
+Object.entries(namedExports).forEach(([ exportName, exported ]) => {
+	if (exportName !== 'default') component[ exportName ] = exported;
 });
 
 export default component as typeof component & Exclude<typeof namedExports, 'default'>;
