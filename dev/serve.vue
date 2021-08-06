7fb908b52c62e8567add369c1df30a406983789c
Volker Nauruhn <684302+razorness@users.noreply.github.com>
2021-08-06 17:21:06 +0200

    dev view changed

--- dev/serve.vue
+++ dev/serve.vue
@@ -63,3 +63,9 @@
 	});
 </script>
 
+<style>
+	body {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: 12px;
+	}
+</style>
