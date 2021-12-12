3ef6528368f3344ade01bee8873d9750a47bbe04
Volker Nauruhn <684302+razorness@users.noreply.github.com>
2021-12-12 05:09:50 +0100

    adding docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,13 +1,13 @@
 import type { DefaultThemeOptions } from 'vuepress';
 import { defineUserConfig } from 'vuepress';
 
-const isProd = process.env.NODE_ENV === 'production';
+const isPublish = process.env.IS_PUBLISH === 'yes';
 
 export default defineUserConfig<DefaultThemeOptions>({
 	lang       : 'en-US',
 	title      : 'Calendar Heatmap',
 	description: 'Vue 3 Calendar Heatmap',
-	base       : isProd ? '/vue3-calendar-heatmap/' : '/',
+	base       : isPublish ? '/vue3-calendar-heatmap/' : '/',
 	themeConfig: {
 		logo: 'https://vuejs.org/images/logo.png'
 	}
